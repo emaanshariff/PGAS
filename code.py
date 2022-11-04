00fd998c680e53146b1d0d22c642c3698a1e0e09
emaanshariff <113933566+emaanshariff@users.noreply.github.com>
2022-11-04 13:56:34 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+#imports from library necessary for function
 import neopixel
 import board
 import time
@@ -6,8 +7,10 @@ import digitalio as dio
 import touchio
 
 
+#define neopixel
 np = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.5, auto_write=False)
 
+#defining button, touch pads, and led
 button_a = dio.DigitalInOut(board.BUTTON_A)
 button_a.direction = dio.Direction.INPUT
 button_a.pull = dio.Pull.DOWN
@@ -18,26 +21,43 @@ touch4 = touchio.TouchIn(board.A2)
 led = dio.DigitalInOut(board.LED)
 led.direction = dio.Direction.OUTPUT
 
+#stores randomized numeric values for randomization of displaying colors
 color_list = []
 
+#stores user input in numeric values
 user_color = 0
+#keeps track of index in color_list in
 count = 0
 
-check = True
+#variable to test if the game should start
 game_start = False
+#variable to test if button_a was pressed
 button_start = False
 
+#creating variables for different colors displayed in the game
 red = [255, 0, 0]
 green = [0, 255, 0]
 blue = [0, 0, 255]
 yellow = [255, 255, 0]
 black = [0, 0, 0]
 
-#adds a random number to color_list to display a color
+
+"""
+Function name: add_color_list
+Description:adds a random number to color_list to represent a color
+Parameters: list - a list to append random numbers to (mainly used for color_list)
+Return value: none
+"""
 def add_color_list(list):
     list.append(random.randint(1, 4))
 
 
+"""
+Function name: correct
+Description: flashes a green light for when the user gets a sequence right
+Parameters: delay - time delayed between neopixel lights (floating point)
+Return value: none
+"""
 def correct(delay=0.1):
     for i in range(2):
         np.fill(green)
@@ -48,6 +68,12 @@ def correct(delay=0.1):
         time.sleep(delay)
 
 
+"""
+Function name: wrong
+Description: flashes a red light for when the user gets a sequence wrong
+Parameters: delay - time delayed between neopixel lights (floating point)
+Return value: none
+"""
 def wrong(delay=0.1):
     for i in range(2):
         np.fill(red)
@@ -58,6 +84,12 @@ def wrong(delay=0.1):
         time.sleep(delay)
 
 
+"""
+Function name: display_color
+Description: checks value from add_color_list() and relates it to a color and displays the color
+Parameters: list - a list to append random numbers to (mainly used for color_list); delay - time delayed between neopixel lights (floating point)
+Return value: none
+"""
 def display_color(list, delay=0.2):
     for item in list:
         if item == 1:
@@ -118,6 +150,12 @@ def display_color(list, delay=0.2):
             time.sleep(delay)
 
 
+"""
+Function name: compare_lists
+Description: compares the user input to the color_list to check if the user remembered the right color; is used primarily in user_input 
+Parameters: user - user input from user_input; color - color_list
+Return value: True or False
+"""
 def compare_lists(user, color):
     global count
     if user == color[count]:
@@ -127,6 +165,12 @@ def compare_lists(user, color):
     else:
         return False
 
+"""
+Function name: reset
+Description: resets variables and lists to default when the game is over
+Parameters: none
+Return value: none
+"""    
 def reset():
     global color_list, game_start
     color_list = []
